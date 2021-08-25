d8312c75af109215a7620d733bac44a335c90bbd
Ilgwon Ha <haik0925@gmail.com>
2021-08-24 23:09:22 -0700

    following day 2

--- src/win32_handmade.cpp
+++ src/win32_handmade.cpp
@@ -1,6 +1,44 @@
 #include <Windows.h>
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                     LPSTR lpCmdLine, int nCmdShow) {
+LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam,
+                                    LPARAM LParam) {
+  LRESULT Result = 0;
+
+  switch (Message) {
+  case WM_SIZE: {
+    OutputDebugStringA("WM_SIZE\n");
+  } break;
+
+  case WM_DESTROY: {
+    OutputDebugStringA("WM_DESTROY\n");
+  } break;
+
+  case WM_CLOSE: {
+    OutputDebugStringA("WM_CLOSE\n");
+  } break;
+
+  case WM_ACTIVATEAPP: {
+    OutputDebugStringA("WM_ACTIVATEAPP\n");
+  } break;
+
+  default: {
+    Result = DefWindowProcA(Window, Message, WParam, LParam);
+  } break;
+  }
+
+  return Result;
+}
+
+int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance,
+                     LPSTR CommandLine, int ShowCode) {
+  WNDCLASS WindowClass = {};
+
+  // TODO(casey): Check if HREDRAW/VREDRAW/OWNDC still matter
+  WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+  WindowClass.lpfnWndProc = MainWindowCallback;
+  WindowClass.hInstance = Instance;
+  // WindowClass.hIcon
+  WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
   return 0;
 }

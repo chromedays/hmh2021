1677f08859d0f775750499cb244b22fe9a1daac1
Ilgwon Ha <haik0925@gmail.com>
2021-08-31 00:09:14 -0700

    following day 2

--- src/win32_handmade.cpp
+++ src/win32_handmade.cpp
@@ -21,6 +21,23 @@ LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam,
     OutputDebugStringA("WM_ACTIVATEAPP\n");
   } break;
 
+  case WM_PAINT: {
+    PAINTSTRUCT Paint;
+    HDC DeviceContext = BeginPaint(Window, &Paint);
+    int X = Paint.rcPaint.left;
+    int Y = Paint.rcPaint.top;
+    int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+    int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+    static DWORD Operation = WHITENESS;
+    if (Operation == WHITENESS) {
+      Operation = BLACKNESS;
+    } else {
+      Operation = WHITENESS;
+    }
+    PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+    EndPaint(Window, &Paint);
+  } break;
+
   default: {
     Result = DefWindowProcA(Window, Message, WParam, LParam);
   } break;
@@ -39,6 +56,29 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance,
   WindowClass.hInstance = Instance;
   // WindowClass.hIcon
   WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+  if (RegisterClassA(&WindowClass)) {
+    HWND WindowHandle = CreateWindowExA(
+        0, WindowClass.lpszClassName, "Handmade Hero",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, Instance, 0);
+    if (WindowHandle) {
+      MSG Message;
+      for (;;) {
+        BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+        if (MessageResult > 0) {
+          TranslateMessage(&Message);
+          DispatchMessage(&Message);
+        } else {
+          break;
+        }
+      }
+    } else {
+      // TODO(casey): Logging
+    }
+  } else {
+    // TODO(casey): Logging
+  }
 
   return 0;
 }

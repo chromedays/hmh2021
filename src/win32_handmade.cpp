c6882e82ceefd65bc3746a568aab7c5665c27fee
Ilgwon Ha <haik0925@gmail.com>
2021-09-19 22:25:08 -0400

    almost finished day 3

--- src/win32_handmade.cpp
+++ src/win32_handmade.cpp
@@ -1,24 +1,73 @@
 #include <Windows.h>
 
-LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam,
-                                    LPARAM LParam) {
+#define internal static
+#define local_persist static
+#define global_variable static
+
+// TODO(casey): This is global for now.
+global_variable bool Running;
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+internal void Win32ResizeDIBSection(int Width, int Height) {
+  // TODO(casey): Bulletproof this.
+  // Maybe don't free first, free after. then free first if that fails.
+
+  if (BitmapHandle) {
+    DeleteObject(BitmapHandle);
+  }
+
+  if (!BitmapDeviceContext) {
+    // TODO(casey): Should we recreate these under certain special
+    // circumstances?
+    BitmapDeviceContext = CreateCompatibleDC(0);
+  }
+
+  BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+  BitmapInfo.bmiHeader.biWidth = Width;
+  BitmapInfo.bmiHeader.biHeight = Height;
+  BitmapInfo.bmiHeader.biPlanes = 1;
+  BitmapInfo.bmiHeader.biBitCount = 32;
+  BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+  BitmapHandle = CreateDIBSection(BitmapDeviceContext, &BitmapInfo,
+                                  DIB_RGB_COLORS, &BitmapMemory, 0, 0);
+}
+
+internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width,
+                                int Height) {
+  BITMAPINFO BitmapInfo;
+  StretchDIBits(DeviceContext, X, Y, Width, Height, X, Y, Width, Height,
+                BitmapMemory, &BitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+}
+
+LRESULT CALLBACK Win32MainWindowCallback(HWND Window, UINT Message,
+                                         WPARAM WParam, LPARAM LParam) {
   LRESULT Result = 0;
 
   switch (Message) {
   case WM_SIZE: {
-    OutputDebugStringA("WM_SIZE\n");
-  } break;
-
-  case WM_DESTROY: {
-    OutputDebugStringA("WM_DESTROY\n");
+    RECT ClientRect;
+    GetClientRect(Window, &ClientRect);
+    int Width = ClientRect.right - ClientRect.left;
+    int Height = ClientRect.bottom - ClientRect.top;
+    Win32ResizeDIBSection(Width, Height);
   } break;
 
   case WM_CLOSE: {
-    OutputDebugStringA("WM_CLOSE\n");
+    // TODO(casey): Handle this with a message to the user?
+    Running = false;
   } break;
 
   case WM_ACTIVATEAPP: {
     OutputDebugStringA("WM_ACTIVATEAPP\n");
+  } break;
+
+  case WM_DESTROY: {
+    // TODO(casey): Handle this as an error - recreate window?
+    Running = false;
   } break;
 
   case WM_PAINT: {
@@ -28,13 +77,7 @@ LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam,
     int Y = Paint.rcPaint.top;
     int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
     int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-    static DWORD Operation = WHITENESS;
-    if (Operation == WHITENESS) {
-      Operation = BLACKNESS;
-    } else {
-      Operation = WHITENESS;
-    }
-    PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+    Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
     EndPaint(Window, &Paint);
   } break;
 
@@ -51,7 +94,7 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance,
   WNDCLASS WindowClass = {};
 
   // TODO(casey): Check if HREDRAW/VREDRAW/OWNDC still matter
-  WindowClass.lpfnWndProc = MainWindowCallback;
+  WindowClass.lpfnWndProc = Win32MainWindowCallback;
   WindowClass.hInstance = Instance;
   // WindowClass.hIcon
   WindowClass.lpszClassName = "HandmadeHeroWindowClass";
@@ -62,8 +105,9 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance,
         WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT,
         CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, Instance, 0);
     if (WindowHandle) {
-      MSG Message;
-      for (;;) {
+      Running = true;
+      while (Running) {
+        MSG Message;
         BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
         if (MessageResult > 0) {
           TranslateMessage(&Message);
